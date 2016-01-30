2f726821b6d410817a2355735cd7ca80f78fce32
getnamo <getnamo@getnamo.com>
2016-01-30 14:11:14 +0000

    Initial Commit
    -Plugin Template
    -Icon
    -Git setup
    -License

---
+++ Source/BodyState/Private/BodyStateBPLibrary.cpp
@@ -0,0 +1,16 @@
+// Copyright 1998-2016 Epic Games, Inc. All Rights Reserved.
+
+#include "BodyStatePrivatePCH.h"
+#include "BodyStateBPLibrary.h"
+
+UBodyStateBPLibrary::UBodyStateBPLibrary(const FObjectInitializer& ObjectInitializer)
+: Super(ObjectInitializer)
+{
+
+}
+
+float UBodyStateBPLibrary::BodyStateSampleFunction(float Param)
+{
+	return -1;
+}
+
